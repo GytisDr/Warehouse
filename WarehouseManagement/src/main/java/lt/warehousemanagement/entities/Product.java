4e3a77d4ea89feed7c5b3ee4c69670e6cc6c6b17
GytisDr <gytis.driauciunas@gmail.com>
2022-08-17 14:30:12 +0300

    Created project

---
+++ WarehouseManagement/src/main/java/lt/warehousemanagement/entities/Product.java
@@ -0,0 +1,77 @@
+package lt.warehousemanagement.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="product")
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	int id;
+	
+	String name;
+	String description;
+	String location;
+	double price;
+	
+	public Product() {}
+
+	public Product(String name, String description, String location, double price) {
+		this.name = name;
+		this.description = description;
+		this.location = location;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", description=" + description + ", location=" + location
+				+ ", price=" + price + "]";
+	}
+
+}
