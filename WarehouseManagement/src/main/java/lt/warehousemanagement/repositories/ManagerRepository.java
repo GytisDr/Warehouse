eb97c3b33cdcf72375c6bb45a36d5a5ac189e8af
GytisDr <110804417+GytisDr@users.noreply.github.com>
2022-09-01 15:51:52 +0300

    Delete ManagerRepository.java
    Removed from project

--- WarehouseManagement/src/main/java/lt/warehousemanagement/repositories/ManagerRepository.java
+++
@@ -1,9 +0,0 @@
-package lt.warehousemanagement.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import lt.warehousemanagement.entities.Manager;
-
-public interface ManagerRepository extends JpaRepository<Manager, Integer> {
-
-}
