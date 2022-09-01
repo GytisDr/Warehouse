ad87c3ad0ea07cab75810ed499049aedc6c81b10
GytisDr <110804417+GytisDr@users.noreply.github.com>
2022-09-01 15:52:01 +0300

    Delete RoleRepository.java
    Removed from project

--- WarehouseManagement/src/main/java/lt/warehousemanagement/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package lt.warehousemanagement.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import lt.warehousemanagement.entities.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Integer>{
-
-}
