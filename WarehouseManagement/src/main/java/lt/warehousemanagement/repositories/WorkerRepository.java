89e6d5ab8da3cb12ced4f3b185aeb63797174052
GytisDr <110804417+GytisDr@users.noreply.github.com>
2022-09-01 15:52:25 +0300

    Delete WorkerRepository.java
    Removed from project

--- WarehouseManagement/src/main/java/lt/warehousemanagement/repositories/WorkerRepository.java
+++
@@ -1,9 +0,0 @@
-package lt.warehousemanagement.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import lt.warehousemanagement.entities.Worker;
-
-public interface WorkerRepository extends JpaRepository<Worker, Integer> {
-
-}
